37c46ce88778441292ba3354918704ed64b1c820
Виктор <eee@mail.ru>
2024-01-06 12:56:03 +0300

    yes

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -3,9 +3,8 @@ package com.epam.mjc.io;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 
 
 public class FileReader {
@@ -13,6 +12,7 @@ public class FileReader {
     public Profile getDataFromFile(File file) {
         StringBuilder str = new StringBuilder();
         boolean fl = false;
+        String[] par = new String[4];
 
         try (FileInputStream inputStream = new FileInputStream(file.getAbsolutePath())) {
             int ch;
@@ -26,12 +26,13 @@ public class FileReader {
                 }
             }
 
-            String[] par = new String[4];
             par = str.toString().trim().split(" ");
-
-            return new Profile(par[0], Integer.valueOf(par[1]), par[2], Long.valueOf(par[3]));
+        } catch (FileNotFoundException e) {
+            e.fillInStackTrace();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.fillInStackTrace();
         }
+
+        return new Profile(par[0], Integer.valueOf(par[1]), par[2], Long.valueOf(par[3]));
     }
 }
